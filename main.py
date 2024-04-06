c3c1a9ac264da02d6cd72d5a59aa5d4785fbcaee
Matheus Cantarutti <cantaruttim@outlook.com>
2024-04-06 12:35:14 -0300

    bd connection

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import uvicorn
 from fastapi import FastAPI
 
 from contas_a_pagar_e_contas_a_receber.router import contas_a_pagar_e_contas_a_receber_routers
+from shared.database import engine, Base
+
+from contas_a_pagar_e_contas_a_receber.models.contas_a_pagar_e_contas_a_receber_model import ContaPagarReceber
+
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
@@ -13,6 +20,7 @@ def home():
 
 # incluimos essa nova rota no main
 app.include_router(contas_a_pagar_e_contas_a_receber_routers.router)
+
 
 if __name__ == "__main__":
     uvicorn.run(app)

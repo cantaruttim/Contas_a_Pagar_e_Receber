d003a28fc22b5c825d2267c7149f3ef74910c272
Matheus Cantarutti <cantaruttim@outlook.com>
2024-04-09 20:30:36 -0300

    testing alembic

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
-
 from contas_a_pagar_e_contas_a_receber.router import contas_a_pagar_e_contas_a_receber_routers
 from shared.database import engine, Base
-
 from contas_a_pagar_e_contas_a_receber.models.contas_a_pagar_e_contas_a_receber_model import ContaPagarReceber
 
-Base.metadata.create_all(bind=engine)
-
+# Base.metadata.drop_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
--- shared/database.py
+++ shared/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 special_argument = '%40' # at = @
+
 SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://postgres:12102021{special_argument}@localhost:5432/contas_a_pagar_e_contas_a_receber'
 
 engine = create_engine(
